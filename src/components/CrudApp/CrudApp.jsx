a5e551d2785eb7bcb5583d53617541b3acac1273
Mike2020x <62442141+Mike2020x@users.noreply.github.com>
2023-05-07 17:51:08 -0500

    Funcionando metodos patch & delete

--- src/components/CrudApp/CrudApp.jsx
+++ src/components/CrudApp/CrudApp.jsx
@@ -1,26 +1,7 @@
 import React, { useState, useEffect } from "react";
 import CrudForm from "../CrudForm/CrudForm";
 import CrudTable from "../CrudTable/CrudTable";
-import "./CrudApp.scss"
-
-/*const initialDB = [
-  {
-    id: 1,
-    name: "Michael",
-    cargo: "FullStack Developer",
-    telefono: 3222322232,
-    email: "mgt@gmail.com",
-    link: "https://github.com/Mike2020x",
-  },
-  {
-    id: 2,
-    name: "Juan",
-    cargo: "FullStack Developer",
-    telefono: 51921657652,
-    email: "cabellosalirrosas@gmail.com",
-    link: "https://github.com/juanxavier357",
-  },
-];*/
+import "./CrudApp.scss";
 
 function CrudApp() {
   const [db, setDb] = useState([]);
@@ -28,60 +9,104 @@ function CrudApp() {
   const [dataToEdit, setDataToEdit] = useState(null);
 
   const handleReadAll = async () => {
-    const url = 'https://api-proyecto-en-parejas.onrender.com/api/members'
+    const url = "https://api-proyecto-en-parejas.onrender.com/api/members";
     const config = {
-      method: 'GET',
-    }
+      method: "GET",
+    };
     try {
-      const response = await fetch(url, config)
-      const data = await response.json()
-      setDb(data) // asigna el valor al estado y genera un nuevo renderizado (pintado)
+      const response = await fetch(url, config);
+      const data = await response.json();
+      setDb(data); // asigna el valor al estado y genera un nuevo renderizado (pintado)
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   useEffect(() => {
-    handleReadAll()
-  }, [])
+    handleReadAll();
+  }, []);
 
   const createData = async (form) => {
     form.id = Date.now();
     //console.log(data);
-    const url = 'https://api-proyecto-en-parejas.onrender.com/api/members'
+    const url = "https://api-proyecto-en-parejas.onrender.com/api/members";
     const config = {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-type': 'application/json; charset=UTF-8',
+        "Content-type": "application/json; charset=UTF-8",
       },
       body: JSON.stringify(form),
-    }
+    };
 
     try {
-      const response = await fetch(url, config)
-      const data = await response.json()
+      const response = await fetch(url, config);
+      const data = await response.json();
       setDb([...db, data]);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   };
 
-  const updateData = (form) => {
-    let newForm = db.map((el) => (el.id === form.id ? form : el));
-    setDb(newForm);
+  // const updateData = (form) => {
+  //   let newForm = db.map((el) => (el.id === form.id ? form : el));
+  //   setDb(newForm);
+  // };
+
+  // const deleteData = (id) => {
+  //   let isDelete = confirm(
+  //     `¿Estas seguro de eliminar el registro con el id '${id}'?`
+  //   );
+  //   if (isDelete) {
+  //     let newData = db.filter((el) => el.id !== id);
+  //     setDb(newData);
+  //   } else {
+  //     return;
+  //   }
+  // };
+  const updateData = async (form) => {
+    const url = `https://api-proyecto-en-parejas.onrender.com/api/members/${form.id}`;
+    const config = {
+      method: "PATCH",
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+      body: JSON.stringify(form),
+    };
+
+    try {
+      const response = await fetch(url, config);
+      const data = await response.json();
+      const updatedDb = db.map((el) => (el.id === data.id ? data : el));
+      setDb(updatedDb);
+      setDataToEdit(null);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
-  const deleteData = (id) => {
+  const deleteData = async (id) => {
     let isDelete = confirm(
       `¿Estas seguro de eliminar el registro con el id '${id}'?`
     );
     if (isDelete) {
-      let newData = db.filter((el) => el.id !== id);
-      setDb(newData);
-    } else {
-      return;
+      const url = `https://api-proyecto-en-parejas.onrender.com/api/members/${id}`;
+      const config = {
+        method: "DELETE",
+      };
+
+      try {
+        const response = await fetch(url, config);
+        if (response.ok) {
+          const newData = db.filter((el) => el.id !== id);
+          setDb(newData);
+        } else {
+          console.log(`Error al eliminar el registro con el id '${id}'`);
+        }
+      } catch (error) {
+        console.log(error);
+      }
     }
-  }
+  };
 
   return (
     <div className="Content__Forms">
